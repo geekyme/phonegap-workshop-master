4a44219e5078585678035f663f28e3e5c79d591b
Shawn Lim Quan Cheng <shawnlim89@live.com>
2013-06-02 16:51:52 +0800

    styling and structuring of code

--- js/main.js
+++ js/main.js
@@ -1,24 +1,11 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
     initialize: function() {
         var self = this;
         this.store = new LocalStorageStore(function(){
-            self.showAlert('Store Initialized', 'Info');
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        
     },
 
     showAlert: function(message, title){
