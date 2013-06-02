fe37cc10dd84da5b005a72b76d6987a7112bdc0a
Shawn Lim Quan Cheng <shawnlim89@live.com>
2013-06-02 17:18:59 +0800

    scrolling

--- js/HomeView.js
+++ js/HomeView.js
@@ -5,8 +5,6 @@ var HomeView = function(store) {
         this.el = $('<div/>');
         this.el.on('keyup', '.search-key', this.findByName);
     };
- 
-    this.initialize();
 
     this.render = function() {
         this.el.html(HomeView.template());
@@ -17,7 +15,16 @@ var HomeView = function(store) {
 	    store.findByName($('.search-key').val(), function(employees) {
 	        $('.employee-list').html(HomeView.liTemplate(employees));
 	    });
+        if(self.iscroll){
+            console.log('Refresh iScroll');
+            self.iscroll.refresh();
+        }else{
+            console.log('New iScroll');
+            self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false})
+        }
 	};
+
+    this.initialize();    
 }
 
 HomeView.template = Handlebars.compile($("#home-tpl").html());
--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
 var app = {
 
-    initialize: function() {
-        var self = this;
-        this.store = new LocalStorageStore(function(){
-            $('body').html(new HomeView(self.store).render().el);
-        });
-        
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     },
 
-    showAlert: function(message, title){
-        if(navigator.notification){
-            navigator.notification.alert(message, null, title, 'OK');
-        }else{
-            alert(title ? (title + ": "+ message): message);
-        }
+    registerEvents: function() {
+        $('body').on('mousedown', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
+        });
+        $('body').on('mouseup', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
+    },
+
+    initialize: function() {
+        var self = this;
+        this.registerEvents();
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
+        });
     }
 
 };
